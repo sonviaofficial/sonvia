bfd5d59e8a7c116fc6ad59db3c2dea13517cd4cb
sonviaofficial <sonviaofficial@gmail.com>
2026-08-11 00:02:49 +0100

    Connect producer button to registration page

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const producerBtn = document.querySelectorAll('button')[0];
 const artistBtn = document.querySelectorAll('button')[1];
 
 producerBtn.addEventListener('click', () => {
-    alert('Producer registration page coming soon on SONVIA!');
+    window.location.href = 'producer.html';
 });
 
 artistBtn.addEventListener('click', () => {
