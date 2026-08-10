ffd8dc9ff32c07be0546e9aa10717d287ac27df1
sonviaofficial <sonviaofficial@gmail.com>
2026-08-10 21:27:17 +0100

    Add event listeners for producer and artist buttons

---
+++ script.js
@@ -0,0 +1,10 @@
+const producerBtn = document.querySelectorAll('button')[0];
+const artistBtn = document.querySelectorAll('button')[1];
+
+producerBtn.addEventListener('click', () => {
+    alert('Producer registration page coming soon on SONVIA!');
+});
+
+artistBtn.addEventListener('click', () => {
+    alert('Artist registration page coming soon on SONVIA!');
+});
