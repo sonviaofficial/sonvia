2759f21efc4fd80d87bebfd35b7d7ee518d8bd15
sonviaofficial <sonviaofficial@gmail.com>
2026-09-02 23:51:47 +0100

    Fix artist dashboard redirect

--- login.js
+++ login.js
@@ -6,12 +6,20 @@ loginForm.addEventListener('submit', function (e) {
     const role = document.querySelector('select').value;
 
     if (role === 'Artist') {
-        window.location.href = "dashboard.html";
+
+        window.location.href = "artist-dashboard.html";
+
     } else if (role === 'Producer') {
+
         window.location.href = 'dashboard.html';
+
     } else if (role === 'Admin') {
+
         window.location.href = 'admin.html';
+
     } else {
+
         alert('Please select a role');
+
     }
 });
